1a8109e2998b2922892b72b234f33a5c4a56531f
layzhang <layzhang@pano.im>
2020-10-09 20:08:58 +0800

    update self module

--- modules.go
+++ modules.go
@@ -10,5 +10,5 @@ import (
 	_ "github.com/gliderlabs/logspout/transports/tcp"
 	_ "github.com/gliderlabs/logspout/transports/tls"
 	_ "github.com/gliderlabs/logspout/transports/udp"
-	_ "github.com/fsantagostinobietti/logspout-file-module"
+	_ "github.com/Tsukiand/logspout-file-module"
 )
